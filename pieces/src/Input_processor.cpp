0614a7edcfb92ccdbe4c6f4f132f5299b336367d
optifat <pavsmel@mail.ru>
2020-07-05 14:22:14 +0300

    replacing arrays with int64_t

--- pieces/src/Input_processor.cpp
+++ pieces/src/Input_processor.cpp
@@ -9,7 +9,32 @@
 #include "../include/King_move.h"
 
 void Input_processor::readMove(Board *board, std::string move) {
-    if (move[0] >= 'a' and move[0] <= 'h') {
+
+    if (move[0] == 'O') {
+        Castling nextMove;
+        nextMove.makeMove(board, move);
+        board->editEnPassant(-1);
+        return;
+    }
+
+    if(move.length() != 5){
+        std::cerr << "Wrong move input (length) \n";
+        return;
+    } else if(move[2] != '-' && move[2] != 'x'){
+        std::cerr << "Wrong move input (not - or x)\n";
+        return;
+    } else if(move[0] == move[3] || move[1] == move[4]){
+        std::cerr << "Impossible move";
+        return;
+    } else if(move[0]>'h' || move[0]<'a' || move[3]>'h' || move[3]<'a' || move[1]>'8' || move[1]<'1' || move[4]>'8' || move[4]<'1'){
+        std::cerr << "Wrong input";
+        return;
+    }
+
+    int initSquare = move[1] - 'a' + 8*(move[2] - '1');
+    int endSquare = move[4] - 'a' + 8*(move[5] - '1');
+
+    if (move[0] >= 'a' && move[0] <= 'h') {
         Pawn_move nextMove;
         if(nextMove.openingPin(board, move)){
             std::cerr << "This piece is pinned\n";
@@ -40,6 +65,10 @@ void Input_processor::readMove(Board *board, std::string move) {
         Bishop_move nextMove;
         if(nextMove.openingPin(board, move.substr(1, 5))){
             std::cerr << "This piece is pinned\n";
+            return;
+        }
+        if(!board->bishops[initSquare] or !board->showCurrentColor()[initSquare]) {
+            std::cerr << "No bishop on " << move[0] << move[1] << "\n";
             return;
         }
         nextMove.makeMove(board, move.substr(1, 5));
