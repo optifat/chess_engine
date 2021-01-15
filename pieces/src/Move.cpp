1b9861c9b53ca779f90989b6f03c77b18004665b
Pavel Smelov <pavsmel@mail.ru>
2021-01-16 01:33:43 +0300

    fixed #4 issue

--- pieces/src/Move.cpp
+++ pieces/src/Move.cpp
@@ -5,53 +5,55 @@ Move::Move() = default;
 
 //void Move::makeMove(Board *board, int initSquare, int endSquare, bool take){};
 
-bool Move::openingPin(Board *board, int initSquare, int endSquare){
+bool Move::openingPin(Board * position, int initSquare, int endSquare){
+
+    auto board = *position;
 
     int kingPos = -1;
     for(int i = 0; i <= 63; i++){
-        if(board->kingCheck(i) && board->currentColorCheck(i)){
+        if(board.kingCheck(i) && board.currentColorCheck(i)){
             kingPos = i;
             break;
         }
     }
 
-    if(!board->currentColorCheck(initSquare)){
+    if(!board.currentColorCheck(initSquare)){
         std::cerr << "No piece with current color here\n";
         return false;
     }
 
-    if(!board->isPinned(initSquare)){
+    if(!board.isPinned(initSquare)){
         return false;
     }
-    else if(!board->anotherColorCheck(endSquare)){
-        board->updateCurrentColor(initSquare, endSquare);
-        //board->showCurrentColor()[initSquare] = false;
-        //board->showCurrentColor()[endSquare] = true;
-        if(board->fieldIsAttacked(kingPos)){
-            board->updateCurrentColor(endSquare, initSquare);
-            //board->showCurrentColor()[initSquare] = true;
-            //board->showCurrentColor()[endSquare] = false;
+    else if(!board.anotherColorCheck(endSquare)){
+        board.updateCurrentColor(initSquare, endSquare);
+        //board.showCurrentColor()[initSquare] = false;
+        //board.showCurrentColor()[endSquare] = true;
+        if(board.fieldIsAttacked(kingPos)){
+            board.updateCurrentColor(endSquare, initSquare);
+            //board.showCurrentColor()[initSquare] = true;
+            //board.showCurrentColor()[endSquare] = false;
             return true;
         }
         else{
-            board->updateCurrentColor(endSquare, initSquare);
-            //board->showCurrentColor()[initSquare] = true;
-            //board->showCurrentColor()[endSquare] = false;
+            board.updateCurrentColor(endSquare, initSquare);
+            //board.showCurrentColor()[initSquare] = true;
+            //board.showCurrentColor()[endSquare] = false;
             return false;
         }
     }
 
     else{
-        board->updateCurrentColor(initSquare, endSquare);
-        board->updateAnotherColor(endSquare, -1);
-        if(board->fieldIsAttacked(kingPos)){
-            board->updateCurrentColor(endSquare, initSquare);
-            board->updateAnotherColor(-1, endSquare);
+        board.updateCurrentColor(initSquare, endSquare);
+        board.updateAnotherColor(endSquare, -1);
+        if(board.fieldIsAttacked(kingPos)){
+            board.updateCurrentColor(endSquare, initSquare);
+            board.updateAnotherColor(-1, endSquare);
             return true;
         }
         else{
-            board->updateCurrentColor(endSquare, initSquare);
-            board->updateAnotherColor(-1, endSquare);
+            board.updateCurrentColor(endSquare, initSquare);
+            board.updateAnotherColor(-1, endSquare);
             return false;
         }
     }
