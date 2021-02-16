5e2ecd7ea03c6d1fc25907fe1cab6b44a4b7c798
optifat <pavsmel@mail.ru>
2021-02-16 14:30:28 +0300

    started refactoring before merging

--- main.cpp
+++ main.cpp
@@ -4,61 +4,88 @@
 #include "tree/include/Tree.h"
 #include "tree/include/Evaluator.h"
 
-int main() {
+int main(int argc, char* argv[]) {
 
-    std::string FEN = "K4BB1/1Q6/5p2/8/2R2r1r/N2N2q1/kp1p1p1p/b7 w - - 0 1";
-    Board board("3KRB2/5p2/5P2/P2p1p2/3k1p2/N2p1P2/3P4/8 w - - 0 1");
-    std::cout << "Print pawn move (e2-e4 for example), the program should return initial and final pieces on these squares \n";
-    std::cout << "Print stop to exit the program \n";
-    Tree tree(std::make_shared<Board>(board));
-    auto start = std::chrono::high_resolution_clock::now();
-    tree.generateTree(4);
-    auto end = std::chrono::high_resolution_clock::now();
-    if (MAX_POS_VAL - abs(tree.getPositionValue()) < 100) {
-        std::cout << "#" << ((tree.getPositionValue() > 0) ? (MAX_POS_VAL - tree.getPositionValue()) : (-MAX_POS_VAL - tree.getPositionValue())) << std::endl;
+    bool analysisMode = true;
+
+    for(int i = 1; i < argc; i++){;
+        auto flag = std::string(argv[i]);
+        if(flag == "-p" || flag == "--playmode"){
+            analysisMode = false;
+        }
+        else{
+            std::cout << "Ignoring unknown " << flag << " flag\n";
+        }
+    }
+
+    std::cout << "Enter FEN (leave empty for starting position)\n";
+    std::string FEN;
+    getline(std::cin, FEN);
+    if(FEN.empty()){
+        FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq";
+    }
+    //std::string FEN = "K4BB1/1Q6/5p2/8/2R2r1r/N2N2q1/kp1p1p1p/b7 w - - 0 1";
+    Board board(FEN);
+
+    if(analysisMode){
+        std::cout << "Enter desired tree depth: \n";
+        int treeDepth = 1;
+        std::cin >> treeDepth;
+        Tree tree(std::make_shared<Board>(board));
+        auto start = std::chrono::high_resolution_clock::now();
+        tree.generateTree(treeDepth);
+        auto end = std::chrono::high_resolution_clock::now();
+        if (MAX_POS_VAL - abs(tree.getPositionValue()) < 100) {
+            std::cout << "#" << ((tree.getPositionValue() > 0) ?
+            (MAX_POS_VAL - tree.getPositionValue()) : (-MAX_POS_VAL - tree.getPositionValue())) << std::endl;
+        }
+        else {
+            std::cout << tree.getPositionValue() << std::endl;
+        }
+        tree.optimalSequence();
+        std::cout << "Total nodes generated: " << tree.totalNodesGenerated() << std::endl;
+        std::cout << "Total tree generation time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+                  << " microseconds" << std::endl;
+        std::cout << 1000000*(double)tree.totalNodesGenerated() / std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() <<
+        " nodes per sec" << std::endl;
     }
     else {
-        std::cout << tree.getPositionValue() << std::endl;
+        std::cout << "Print pawn move (e2-e4 for example), the program should return initial and final pieces on these squares \n";
+        std::cout << "Print stop to exit the program \n";
+        while(true) {
+            board.printInfo();
+            board.showBoard();
+            if(board.checkmate()){
+                std::cout << "Checkmate. ";
+                if(board.whiteOrder()){
+                    std::cout << "Black wins." << std::endl;
+                    return 0;
+                }
+                else{
+                    std::cout << "White wins." << std::endl;
+                    return 0;
+                }
+            }
+            else if(board.stalemate()){
+                std::cout << "Stalemate. Draw.";
+                return 0;
+            }
+            std::cout << board.sideToMove() << std::endl;
+            std::string input;
+            std::cin >> input;
+            if (input == "stop") {
+                break;
+            } else {
+                auto start = std::chrono::high_resolution_clock::now();
+                Input_processor::readMove(&board, input);
+                auto end = std::chrono::high_resolution_clock::now();
+                std::cout << "Duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+                << " microseconds" << std::endl;
+
+            }
+        }
     }
-    tree.optimalSequence();
-    std::cout << "Total nodes generated: " << tree.totalNodesGenerated() << std::endl;
-    std::cout << "Total tree generation time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
-        << " microseconds" << std::endl;
-    std::cout << 1000000*tree.totalNodesGenerated() / std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() << " nodes per sec" << std::endl;
-    /*while(true) {
-        board.printInfo();
-        board.showBoard();
-        if(board.checkmate()){
-            std::cout << "Checkmate. ";
-            if(board.whiteOrder()){
-                std::cout << "Black wins." << std::endl;
-                return 0;
-            }
-            else{
-                std::cout << "White wins." << std::endl;
-                return 0;
-            }
-        }
-        else if(board.stalemate()){
-            std::cout << "Stalemate. Draw.";
-            return 0;
-        }
-        std::cout << board.sideToMove() << std::endl;
-        std::string input;
-        std::cin >> input;
-        if (input == "stop") {
-            break;
-        } else {
-            auto start = std::chrono::high_resolution_clock::now();
-            Input_processor::readMove(&board, input);
-            auto end = std::chrono::high_resolution_clock::now();
-            std::cout << "Duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
-            << " microseconds" << std::endl;
-
-        }
-    }//*/
-
-    //board.showBoard();
-    //std::cout << sizeof(board) << "\n";
+    /*board.showBoard();
+    std::cout << sizeof(board) << "\n";*/
     return 0;
 }
