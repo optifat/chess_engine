596e7669bca1d75eac3d33c4d045166d57eaf8ca
optifat <pavsmel@mail.ru>
2021-03-18 15:17:44 +0300

    changed pieces values

--- tree/src/Evaluator.cpp
+++ tree/src/Evaluator.cpp
@@ -9,22 +9,22 @@
 int Evaluator::evaluatePosition(Board &position) {
     int positionValue = 0;
     if (position.checkmate()) {
-        return position.whiteOrder() ? -MAX_POS_VAL : MAX_POS_VAL; // float isn't precise enough, so FLT_MAX - 1 == FLT_MAX 
+        return position.whiteOrder() ? -MAX_POS_VAL : MAX_POS_VAL;
     }
     /* doesn't work right now
     if (position.stalemate()) {
         return 0;
     }*/
     // counting material
-    positionValue += 9 * __builtin_popcountl(position.queens & position.whitePieces);
-    positionValue -= 9 * __builtin_popcountl(position.queens & position.blackPieces);
-    positionValue += 5 * __builtin_popcountl(position.rooks & position.whitePieces);
-    positionValue -= 5 * __builtin_popcountl(position.rooks & position.blackPieces);
-    positionValue += 3 * __builtin_popcountl(position.bishops & position.whitePieces);
-    positionValue -= 3 * __builtin_popcountl(position.bishops & position.blackPieces);
-    positionValue += 3 * __builtin_popcountl(position.knights & position.whitePieces);
-    positionValue -= 3 * __builtin_popcountl(position.knights & position.blackPieces);
-    positionValue += __builtin_popcountl(position.pawns & position.whitePieces);
-    positionValue -= __builtin_popcountl(position.pawns & position.blackPieces);
+    positionValue += 900 * __builtin_popcountl(position.queens & position.whitePieces);
+    positionValue -= 900 * __builtin_popcountl(position.queens & position.blackPieces);
+    positionValue += 500 * __builtin_popcountl(position.rooks & position.whitePieces);
+    positionValue -= 500 * __builtin_popcountl(position.rooks & position.blackPieces);
+    positionValue += 300 * __builtin_popcountl(position.bishops & position.whitePieces);
+    positionValue -= 300 * __builtin_popcountl(position.bishops & position.blackPieces);
+    positionValue += 300 * __builtin_popcountl(position.knights & position.whitePieces);
+    positionValue -= 300 * __builtin_popcountl(position.knights & position.blackPieces);
+    positionValue += 100 * __builtin_popcountl(position.pawns & position.whitePieces);
+    positionValue -= 100 * __builtin_popcountl(position.pawns & position.blackPieces);
     return positionValue;
 }
